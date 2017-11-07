be605f0a508d2e795357270597c00b03f8f144c1
Unknown <paalginaa@gmail.com>
2017-11-07 12:59:49 +0100

    ddl file ignore, testing data

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,23 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Vehicle vehicle = new Vehicle();
+        Vehicle vehicle1 = new Vehicle("Jármű2");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("carsharePU");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+
+        transaction.begin();
+        em.persist(vehicle);
+        em.persist(vehicle1);
+        transaction.commit();
+
+        em.close();
+        emf.close();
+    }
 }
--- src/main/java/Vehicle.java
+++ src/main/java/Vehicle.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Vehicle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name = "Jármű";
+
+    public Vehicle(){
+    }
+
+    public Vehicle (String name) {
+        this.name = name;
+    }
 }
