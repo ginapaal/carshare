13f85fb9ed5e15c2910cda4c7a394f5ae9771839
Gergely Németh <ngerle@gmail.com>
2017-11-08 00:49:21 +0100

    Add new routes and a user to main

--- src/main/java/com/codecool/carshare/Main.java
+++ src/main/java/com/codecool/carshare/Main.java
@@ -1,5 +1,8 @@
 package com.codecool.carshare;
 
+import com.codecool.carshare.controller.PageController;
+import com.codecool.carshare.model.Customer;
+import com.codecool.carshare.model.SecurePassword;
 import com.codecool.carshare.model.Vehicle;
 import spark.ModelAndView;
 import spark.Request;
@@ -11,29 +14,38 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import java.util.HashMap;
-import java.util.Map;
 
 import static spark.Spark.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InvalidKeySpecException, NoSuchAlgorithmException {
         //default server settings
         exception(Exception.class, (e, req, res) -> e.printStackTrace());
         staticFileLocation("/public");
         port(8888);
-        
+
         get("/a", (Request req, Response res) -> new ThymeleafTemplateEngine().render(new ModelAndView(new HashMap<>(), "index")));
+        get("/register", PageController::register);
+        post("/register", PageController::register);
+        get("/login", PageController::login);
+        post("/login", PageController::login);
 
         Vehicle vehicle = new Vehicle();
         Vehicle vehicle1 = new Vehicle("Jármű2");
+        Customer user = new Customer("gergo", "email@email.com", "aaa");
+
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("carsharePU");
         EntityManager em = emf.createEntityManager();
         EntityTransaction transaction = em.getTransaction();
+
         transaction.begin();
         em.persist(vehicle);
         em.persist(vehicle1);
+        em.persist(user);
         transaction.commit();
         em.close();
         emf.close();
