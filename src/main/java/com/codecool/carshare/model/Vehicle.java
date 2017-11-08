ae1dc3a76690c27b7e4464fa1d1fe7889e626fb7
akosENagy <akos.e.nagy@gmail.com>
2017-11-08 10:16:11 +0100

    Extended vehicle class with a few fields and added namedqueries

--- src/main/java/com/codecool/carshare/model/Vehicle.java
+++ src/main/java/com/codecool/carshare/model/Vehicle.java
@@ -1,22 +1,57 @@
 package com.codecool.carshare.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
+@NamedQueries({
+    @NamedQuery(
+            name = "Vehicle.getAll",
+            query = "SELECT v FROM Vehicle v"
+    ),
+    @NamedQuery(
+            name = "Vehicle.getByType",
+            query = "SELECT v FROM Vehicle v WHERE vehicleType = :type" //TODO
+    )
+})
 @Entity
 public class Vehicle {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    private String name = "Jármű";
+    private String name;
+    private int year;
+    private int numOfSeats;
+    @Enumerated
+    private VehicleType vehicleType;
 
     public Vehicle(){
+
     }
 
-    public Vehicle (String name) {
+    public Vehicle (String name, int year, int numOfSeats, VehicleType vehicleType) {
         this.name = name;
+        this.year = year;
+        this.numOfSeats = numOfSeats;
+        this.vehicleType = vehicleType;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public VehicleType getVehicleType() {
+        return vehicleType;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getNumOfSeats() {
+        return numOfSeats;
     }
 }
