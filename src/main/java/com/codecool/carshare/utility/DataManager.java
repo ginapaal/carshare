09d46b95a8f639c90fbc93c1d80be5ff69ad0a51
Gergely Németh <ngerle@gmail.com>
2017-11-23 20:11:02 +0100

    Add design to reservation form

--- src/main/java/com/codecool/carshare/utility/DataManager.java
+++ src/main/java/com/codecool/carshare/utility/DataManager.java
@@ -8,6 +8,10 @@ import com.codecool.carshare.model.VehicleType;
 import javax.persistence.*;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import static com.codecool.carshare.model.VehicleType.*;
@@ -89,10 +93,25 @@ public class DataManager {
     public void populateTestData() throws InvalidKeySpecException, NoSuchAlgorithmException {
         User owner = new User("gergo", "ngerle@gmail.com", securePassword.createHash("pass"));
         User owner2 = new User("Ödönke", "odon@tokodon.hu", securePassword.createHash("odon"));
-        Vehicle vehicle = new Vehicle("Ödönke kocsija", 1978, 3, Car, "https://www.alamo.com/alamoData/vehicle/bookingCountries/US/CARS/SSAR.doi.320.high.imageLargeThreeQuarterNodePath.png/1508943174788.png");
-        Vehicle vehicle1 = new Vehicle("Ödönke másik kocsija", 1990, 6, Car, "http://www.pngpix.com/wp-content/uploads/2016/06/PNGPIX-COM-Aston-Martin-V12-Zagato-Red-Sports-Car-PNG-Image.png");
-        Vehicle vehicle2 = new Vehicle("Ödönke harmadik kocsija", 1990, 6, Car, "http://www.pngpix.com/wp-content/uploads/2016/06/PNGPIX-COM-Bugatti-Chiron-Blue-Car-PNG-Image.png");
-        Vehicle vehicle3 = new Vehicle("Ödönke negyedik kocsija", 1990, 6, Car, "http://www.pngpix.com/wp-content/uploads/2016/06/PNGPIX-COM-BMW-M2-Coupe-White-Car-PNG-Image.png");
+
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        Date startDate = new Date();
+        Date endDate = new Date();
+        try {
+            startDate = df.parse("2017-11-10");
+            endDate = df.parse("2017-12-01");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        Vehicle vehicle = new Vehicle("Ödönke kocsija", 1978, 3, Car, "https://www.alamo.com/alamoData/vehicle/bookingCountries/US/CARS/SSAR.doi.320.high.imageLargeThreeQuarterNodePath.png/1508943174788.png",
+                startDate, endDate);
+        Vehicle vehicle1 = new Vehicle("Ödönke másik kocsija", 1990, 4, Car, "http://www.pngpix.com/wp-content/uploads/2016/06/PNGPIX-COM-Aston-Martin-V12-Zagato-Red-Sports-Car-PNG-Image.png",
+                startDate, endDate);
+        Vehicle vehicle2 = new Vehicle("Ödönke harmadik kocsija", 1990, 4, Car, "http://www.pngpix.com/wp-content/uploads/2016/06/PNGPIX-COM-Bugatti-Chiron-Blue-Car-PNG-Image.png",
+                startDate, endDate);
+        Vehicle vehicle3 = new Vehicle("Ödönke negyedik kocsija", 1990, 4, Car, "http://www.pngpix.com/wp-content/uploads/2016/06/PNGPIX-COM-BMW-M2-Coupe-White-Car-PNG-Image.png",
+                startDate, endDate);
         owner.addVehicle(vehicle1);
         owner.addVehicle(vehicle2);
         owner.addVehicle(vehicle3);
@@ -107,11 +126,16 @@ public class DataManager {
         persist(vehicle2);
         persist(vehicle3);
 
-        persist(new Vehicle("Egy motor", 2005, 1, Motor, "http://pngimg.com/uploads/motorcycle/motorcycle_PNG5341.png"));
-        persist(new Vehicle("Egy másik motor", 2009, 1, Motor, "http://pngimg.com/uploads/motorcycle/motorcycle_PNG3150.png"));
-        persist(new Vehicle("Batmobile", 1960, 2, Car, "https://cache.popcultcha.com.au/media/catalog/product/cache/1/image/1800x/040ec09b1e35df139433887a97daa66f/m/e/metals-batman-v-superman-batmobile-4-inch-01_1.1498484298.png"));
-        persist(new Vehicle("Kitt", 1980, 4, Car, "http://www.hotwheels-elite.com/diecast-model-cars/images/Image/CATALOGO-A-2010/1-18%20CULT%20CLASSIC%20COLLECTION/X5469%20-%20KITT/X5469_PoP_13_12_w900.png"));
-        persist(new Vehicle("Bobby's first bike", 2002, 1, Bike, "http://www.straydogbicycles.com/images/Specialized_Hotrock.png"));
+        persist(new Vehicle("Egy motor", 2005, 1, Motor, "http://pngimg.com/uploads/motorcycle/motorcycle_PNG5341.png",
+                startDate, endDate));
+        persist(new Vehicle("Egy másik motor", 2009, 1, Motor, "http://pngimg.com/uploads/motorcycle/motorcycle_PNG3150.png",
+                startDate, endDate));
+        persist(new Vehicle("Batmobile", 1960, 2, Car, "https://cache.popcultcha.com.au/media/catalog/product/cache/1/image/1800x/040ec09b1e35df139433887a97daa66f/m/e/metals-batman-v-superman-batmobile-4-inch-01_1.1498484298.png",
+                startDate, endDate));
+        persist(new Vehicle("Kitt", 1980, 4, Car, "http://www.hotwheels-elite.com/diecast-model-cars/images/Image/CATALOGO-A-2010/1-18%20CULT%20CLASSIC%20COLLECTION/X5469%20-%20KITT/X5469_PoP_13_12_w900.png",
+                startDate, endDate));
+        persist(new Vehicle("Bobby's first bike", 2002, 1, Bike, "http://www.straydogbicycles.com/images/Specialized_Hotrock.png",
+                startDate, endDate));
 
     }
 }
