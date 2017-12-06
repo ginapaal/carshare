c461956625db62897742575afc4ec170005f51b1
Gina <paalginaa@gmail.com>
2017-12-06 14:27:18 +0100

    map implementation

--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+function styling() {
 
     $('input#username').on({
         keydown: function (event) {
@@ -27,5 +27,35 @@ $(document).ready(function () {
         $('.upload-profile-pic').addClass('hidden');
         $('.cancel-profile-pic').addClass('hidden');
     });
+}
 
-});
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map-container'), {
+        zoom: 8,
+        // center: {lat: -34.397, lng: 150.644}
+    });
+    var geocoder = new google.maps.Geocoder();
+    geocodeAddress(geocoder, map);
+}
+
+function geocodeAddress(geocoder, resultsMap) {
+    var address = "Budapest";
+    geocoder.geocode({'address': address}, function(results, status) {
+        if (status === 'OK') {
+            resultsMap.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                map: resultsMap,
+                position: results[0].geometry.location
+            });
+        } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+        }
+    });
+}
+
+function main() {
+    styling();
+    initMap();
+}
+
+$(document).ready(main);
