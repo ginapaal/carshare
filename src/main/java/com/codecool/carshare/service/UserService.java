3b6391aba911fa4a02b90fa1153405a2b1890031
Gergely Németh <ngerle@gmail.com>
2017-12-06 12:17:09 +0100

    Populate data through to InitializerBean

--- src/main/java/com/codecool/carshare/service/UserService.java
+++ src/main/java/com/codecool/carshare/service/UserService.java
@@ -80,4 +80,9 @@ public class UserService {
         User user = userRepository.getUserByName((String) session.getAttribute("user"));
         return user;
     }
+
+    public void saveUser(User entity) {
+        userRepository.save(entity);
+    }
+
 }
