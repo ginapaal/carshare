d5f7ddf4c60e8eee25515b1df97cb40fe6138a8f
Gergely Németh <ngerle@gmail.com>
2017-12-07 18:01:56 +0100

    Refactor login tests

--- src/test/java/com/codecool/carshare/service/UserServiceTest.java
+++ src/test/java/com/codecool/carshare/service/UserServiceTest.java
@@ -1,6 +1,8 @@
 package com.codecool.carshare.service;
 
+import com.codecool.carshare.model.User;
 import com.codecool.carshare.repository.UserRepository;
+import com.codecool.carshare.utility.SecurePassword;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +12,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.ui.ExtendedModelMap;
 import org.springframework.ui.Model;
 
+import javax.servlet.http.HttpSession;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
+import java.util.Map;
 
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -36,6 +40,12 @@ public class UserServiceTest {
     @MockBean
     InitializerBean initializerBean;
 
+    @MockBean
+    HttpSession session;
+
+    @MockBean
+    SecurePassword securePassword;
+
     @Test
     public void testRegisterIfEveryInputIsOK() throws InvalidKeySpecException, NoSuchAlgorithmException {
         String username = "gergo";
@@ -59,5 +69,59 @@ public class UserServiceTest {
         userService.register(username, password, confirmPassword, email, model);
 
         assertEquals("Passwords don't match!", model.asMap().get("errorMessage"));
+    }
+
+    @Test
+    public void testLoginSendsErrorMessageIfPasswordIsEmpty() throws InvalidKeySpecException, NoSuchAlgorithmException {
+        String username = "binladen";
+        String password = "";
+        String expectedErrorMessage = "All fields are required";
+
+        Map testData = userService.login(username, password, session);
+
+        assertEquals(expectedErrorMessage, testData.get("errorMessage"));
+    }
+
+    @Test
+    public void testLoginSendsErrorMessageIfUsernameIsEmpty() throws InvalidKeySpecException, NoSuchAlgorithmException {
+        String username = "";
+        String password = "password";
+        String expectedErrorMessage = "All fields are required";
+
+        Map testData = userService.login(username, password, session);
+
+        assertEquals(expectedErrorMessage, testData.get("errorMessage"));
+    }
+
+    @Test
+    public void testLoginSendsErrorMessageIfPasswordIsInvalid() throws InvalidKeySpecException, NoSuchAlgorithmException {
+        String username = "a";
+        String password = "b";
+        String expectedErrorMessage = "Invalid username or password";
+        User user = mock(User.class);
+
+        when(securePassword.isPasswordValid(anyString(), anyString())).thenReturn(false);
+        when(userRepository.getUserByName(anyString())).thenReturn(user);
+        when(user.getPasswordHash()).thenReturn("any");
+
+        Map testData = userService.login(username, password, session);
+
+        assertEquals(expectedErrorMessage, testData.get("errorMessage"));
+    }
+//
+    @Test
+    public void testLoginIfPasswordIsValid() throws InvalidKeySpecException, NoSuchAlgorithmException {
+        String username = "a";
+        String password = "b";
+        User user = mock(User.class);
+
+        when(userRepository.getUserByName(anyString())).thenReturn(user);
+        when(user.getPasswordHash()).thenReturn("any");
+        when(securePassword.isPasswordValid(anyString(), anyString())).thenReturn(true);
+
+        Map testData = userService.login(username, password, session);
+
+        assertEquals(null, testData.get("errorMessage"));
+        assertEquals(username, testData.get("user"));
     }
 }
